d0ba269dce9918c4c7bdb563cf635f00a4afbdee
Samir Kumar Singh <ersamirsingh@gmail.com>
2025-09-07 13:41:25 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import LoginPage from "./Pages/LoginPage";
+import SearchPage from "./Pages/SearchPage";
+import UploadPage from "./Pages/UploadPAge";
+import DocumentPage from "./Pages/DocumentPage";
+import InboxPage from "./Pages/InboxPage";
+import AdminUploadDashboard from "./Pages/AdminUploadDashboard";
+import AdminExecutiveDashboard from "./Pages/AdminExecutiveDashboard";
+
+
+
+export default function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-base-100">
+        {/* Navbar on all pages */}
+        <Navbar />
+
+        <div className="p-4">
+          <Routes>
+            <Route path="/" element={<SearchPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/upload" element={<UploadPage />} />
+            <Route path="/doc/:id" element={<DocumentPage />} />
+            <Route path="/inbox" element={<InboxPage />} />
+            <Route path="/admin/upload" element={<AdminUploadDashboard />} />
+            <Route path="/admin/executive" element={<AdminExecutiveDashboard />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
